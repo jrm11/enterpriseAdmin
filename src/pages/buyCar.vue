2dc3885d66d4ad1a77f978979db059bf5e246ce9
简荣明 <jianronmin@163.com>
2017-12-09 17:17:26 +0800

    modify

--- src/pages/buyCar.vue
+++ src/pages/buyCar.vue
@@ -1,78 +1,46 @@
 <template>
   <div class="w">
-    <div>
-      <ul class="header">
-        <li class=" check">
-          <input type="checkbox" v-model="checkAll">全选
-        </li>
-        <li class=" pic">
-          <p>dd</p>
-        </li>
-        <li class=" name">
-          <p>商品名称</p>
-        </li>
-        <li class=" price">
-          <p>单价</p>
-        </li>
-        <li class=" num">
-          <p>数量</p>
-        </li>
-        <li class=" total">
-          <p>小计</p>
-        </li>
-        <li class=" action">
-          <p>操作</p>
-        </li>
-      </ul>
-      <div v-for="(item ,index) in data" key="index">
-        <div class="list-body clearfix">
-          <div class="list-item">
-            <div class="item-check col">
-              <input type="checkbox" v-model="item.checked" @click="currClick(item,index)">
-            </div>
-          </div>
-          <div class="list-item">
-            <div class="item-pic col">
-              <p :style="{background:item.img}" style="width: 50px; height: 50px"></p>
-            </div>
-          </div>
-          <div class="list-item">
-            <div class="item-name col">
-              <h3>{{item.name}}</h3>
-              <span>{{item.subName}}</span>
-            </div>
-          </div>
-          <div class="list-item">
-            <div class="item-price col">
-              <p>{{item.price | formatMoney}}</p>
-            </div>
-          </div>
-          <div class="list-item">
-            <div class="item-num col">
-              <div class="change-goods-num clearfix">
-                <a type="button" class="minus  fl" @click.prevent="changeMoney(item,-1)">-</a>
-                <input type="text" class="change-num fl" v-model="item.num">
-                <a type="button" class="plus  fl" @click.prevent="changeMoney(item,1)">+</a>
-              </div>
-            </div>
-          </div>
-          <div class="list-item">
-            <div class="item-total col">
-              <p>{{item.price*item.num | formatMoney}}</p>
-            </div>
-          </div>
-          <div class="list-item">
-            <div class="item-action col">
-              <button class="btn-del" @click="del(index)">删除</button>
-            </div>
-          </div>
-        </div>
+    <template v-if="list.length">
+      <div style="background: #fff">
+      <table>
+        <thead>
+        <tr>
+          <th>
+            <input type="checkbox" @click="checkAll" v-model="checked">全选
+          </th>
+          <th>商品名称</th>
+          <th>商品单价</th>
+          <th>商品数量</th>
+          <th>小计</th>
+          <th>操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item,index) in list">
+          <td>
+            <input type="checkbox" v-model="selectList" :value="index" name="selectList" @click="singleCheck(item)">
+          </td>
+          <td>{{item.name}}</td>
+          <td>{{item.price}}</td>
+          <td>
+            <a @click.prevent="changeMoney(index,-1)" :disabled="item.count === 1">-</a>
+            <input type="text" v-model="item.count " style="width: 30px">
+            <a @click.prevent="changeMoney(index,1)">+</a>
+          </td>
+          <td>
+            <p>{{item.price*item.count}}</p>
+          </td>
+          <td>
+            <button @click="del(index)">移除</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <div>总价：{{ totalPrice }}</div>
+      <div>总购买数量： {{ totalNum }}</div>
       </div>
-      <div class="result">
-        <p class="fl">共{{totalNum}}件商品</p>
-        <p class="fr"><span>共</span>{{totalMoney |formatMoney}}<span>元</span></p>
-      </div>
-    </div>
+    </template>
+    <div v-else>购物车为空！</div>
   </div>
 
 </template>
@@ -83,108 +51,79 @@
   export default {
     data () {
       return {
-        data: [],
-        checked: [],
-        totalPrice: 0,
-      }
-    },
-    methods: {
-      //删除
-      del(index){
-        this.data.splice(index, 1);
-      },
-      //数量改变
-      changeMoney(item, index){
-        if (index > 0) {
-          item.num++
-        } else {
-          item.num--;
-          if (item.num <=1) {
-            item.num = 1;
-          }
-        }
-      },
-      //列表checkbox点击
-      currClick(item, index){
-        console.log(item)
-        var _this = this;
-        if (item.checked == false) {
-          this.$set(item, 'checked', true);
-          let total = item.price * item.num;
-          _this.totalPrice.push(total);
-        } else {
-          item.checked = !item.checked;
-          if (item.checked) {
-            this.totalPrice = [];
-            this.data.forEach(function (item, index) {
-              if (item.checked) {
-                let total = item.price * item.num;
-                _this.totalPrice.push(total);
-              }
-            });
-          } else {
-            this.totalPrice = [];
-            this.data.forEach(function (item, index) {
-              if (item.checked) {
-                let total = item.price * item.num;
-                _this.totalPrice.push(total);
-              }
-            });
-          }
-        }
-      }
-    },
-
-    filters: {
-      //价格过滤
-      formatMoney(val){
-        return "￥" + val.toFixed(2);
+        list: [],
+        selectList: [],
+        checked: false
       }
     },
     computed: {
-      totalMoney(){
-        var sum = 0;
-        for (let i = 0; i < this.totalPrice.length; i++) {
-          sum += this.totalPrice[i];
-        }
-
-        return sum;
+      totalPrice () {
+        var total = 0;
+        var _this = this;
+        this.selectList.forEach(function (index) {
+          var data = _this.list[index];
+          total += data.price * data.count;
+        });
+        return total;
       },
       totalNum(){
-        var totalNum = 0;
-        for (let i = 0; i < this.data.length; i++) {
-          totalNum += parseInt(this.data[i].num);
-        }
-        return totalNum;
+        var totalCount = 0;
+        var _this = this;
+        this.selectList.forEach(function (index) {
+          var data = _this.list[index];
+          totalCount += data.count;
+        });
+        return totalCount;
+      }
+    },
+    methods: {
+      singleCheck(item){
+
       },
-      //全选
-      checkAll: {
-        get() {
-          return this.checkedCount == this.data.length;
-        },
-        set(value){
-          var _this = this;
-          if (value) {
-            this.totalPrice = [];
-            this.checked = this.data.map(function (item) {
-              item.checked = true;
-              let total = item.price * item.num;
-              _this.totalPrice.push(total);
-              return item
-            })
-          } else {
-            this.checked = [];
-            this.totalPrice = [];
-            this.data.forEach(function (item, index) {
-              item.checked = false;
-            });
+      changeMoney (index, i) {
+        var item = this.list[index];
+        if (i < 0) {
+          item.count--;
+          if (item.count < 1) {
+            return false;
           }
+        } else {
+          item.count++;
         }
       },
-      checkedCount: {
-        get() {
-          return this.checked.length;
+      del (index) {
+        this.list.splice(index, 1);
+      },
+      checkAll () {
+        var selectList = document.getElementsByName('selectList');
+        var len = selectList.length;
+        if (this.checked) {//反选
+          for (var i = 0; i < len; i++) {
+            var item = selectList[i];
+            item.checked = true;
+            console.log(item);
+          }
+          this.checked = false;
+          this.selectList = [];
         }
+        else {//全选
+          for (i = 0; i < len; i++) {
+            item = selectList[i];
+            if (item.checked === false) {
+              item.checked = true;
+              this.selectList.push(selectList[i].value);
+              console.log(item);
+            }
+          }
+          this.checked = true;
+        }
+      }
+    },
+    filters: {
+      //价格过滤
+      formatMoney(val){
+        alert(val);
+        return "￥" + val.toFixed(2);
       }
     },
     mounted(){
@@ -195,132 +134,37 @@
           img: Mock.mock('@color()'),
           name: '@name',
           subName: '@name',
-          "num|1-20": 20,
-          'price|1-100': 10000
+          "count|1-20": 20,
+          'price|1-100': 10000,
         }));
       }
-      this.data = dataList;
+      this.list = dataList;
     }
   }
 </script>
-<style lang="scss" rel="stylesheet/scss" scoped>
-  @import "../assets/style/app";
+<style>
+  [v-cloak] {
+    display: none;
+  }
 
-  $bgf: #fff;
+  table {
+    background: #fff;
+    border: 1px solid black;
+    border-collapse: collapse;
+    border-spacing: 0;
+    empty-cells: show;
+  }
+
+  th, td {
+    padding: 8px 16px;
+    border: 1px solid black;
+    text-align: center;
+  }
   .w {
     width: 1200px;
     margin: 0 auto;
   }
-
-  .result {
-    padding: 0 20px;
-    height: 60px;
-    line-height: 60px;
-    background: #fff;
-    border-top: 1px solid #ddd
-  }
-
-  .col {
-    height: 50px;
-    vertical-align: middle;
-    display: table-cell;
-  }
-
-  /*thead*/
-  .header {
-    line-height: 60px;
-    height: 60px;
-    background: $bgf;
-    text-align: left;
-    li {
-      float: left;
-    }
-  }
-
-  /*购物车内容*/
-  .list-body {
-    background: #fff;
-    border-top: 1px solid #ddd;
-    padding: 15px 30px 15px 0;
-  }
-
-  .list-item {
-    float: left;
-    background: $bgf;
-    text-align: left;
-    display: table-cell;
-    vertical-align: middle;
-  }
-
-  .check, .item-check {
-    width: 100px;
-    padding-left: 20px;
-  }
-
-  .pic, .item-pic {
-    width: 120px;
-  }
-
-  .name, .item-name {
-    width: 380px;
-  }
-
-  .price, .item-price {
-    width: 150px;
-  }
-
-  .change-goods-num {
-    box-sizing: border-box;
-    height: 38px;
-    line-height: 38px;
-    text-align: center;
-    border: 1px solid #ddd;
-  }
-
-  .minus, .plus {
-    width: 36px;
-    height: 36px;
-    background: #fff;
-    border: none;
-    transition: .2s;
-  }
-
-  .minus:hover, .plus:hover {
-    background: #f0f0f0;
-  }
-
-  .change-num {
-    border: none;
-    text-align: center;
-    width: 46px;
-    height: 36px;
-  }
-
-  .num, .item-num {
-    width: 120px;
-    text-align: center;
-  }
-
-  .total, .item-total {
-    width: 150px;
-    text-align: center;
-  }
-
-  .action, .item-action {
-    width: 120px;
-    text-align: center;
-  }
-
-  .btn-del {
-    padding: 5px 10px;
-    border: none;
-    background: #409EFF;
-    color: #fff;
-    font-size: 14px;
-    transition: .3s;
-  }
-
-  .btn-del:hover {
-    background: #4968ff;
+  th {
+    background-color: gray;
   }
 </style>
